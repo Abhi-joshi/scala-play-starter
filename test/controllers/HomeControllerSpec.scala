fa1fdcea77adb3c64a1ed96aff0d0fa6bb1e35a8
Abhishek Sharma <mail.abhishekjoshi@gmail.com>
2018-02-09 01:36:37 +0530

    Updating the test cases.

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -4,6 +4,7 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.test.CSRFTokenHelper._
 
 /**
  * Add your spec here.
@@ -15,31 +16,24 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
     "render the index page from the application" in {
       val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val request = FakeRequest(GET, "/").withCSRFToken
+      val home = controller.index().apply(request)
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Insert a Customer here:")
     }
 
     "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
+      
+      val request = FakeRequest(GET, "/").withCSRFToken
       val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Insert a Customer here:")
     }
   }
 }
